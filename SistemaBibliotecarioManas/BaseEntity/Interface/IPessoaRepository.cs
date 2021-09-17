c5c69d50e0f02be8e8437784fa5e22cc3d573b57
Mabel Kruger de Souza <mabelksouza@gmail.com>
2021-09-17 17:07:19 -0300

    Finalização da Interface e do repository

---
+++ SistemaBibliotecarioManas/BaseEntity/Interface/IPessoaRepository.cs
@@ -0,0 +1,14 @@
+﻿using SistemaBiblitecarioManas.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBiblitecarioManas.Entities.Interface
+{
+    public interface IPessoaRepository : IGenericRepository<PessoaEntity>
+    {
+        Task<bool> PessoaJaExiste(string cpf);
+        Task<bool> PessoaJaExisteComEsseCpf(string cpf, int id);
+    }
+}
--- SistemaBibliotecarioManas/SistemaBibliotecarioManas.Infra/Repository/PessoaRepository/PessoaRepository.cs
+++ SistemaBibliotecarioManas/SistemaBibliotecarioManas.Infra/Repository/PessoaRepository/PessoaRepository.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
+using Microsoft.EntityFrameworkCore;
+using SistemaBibliotecarioManas.Infra.Repository.GenericRepository;
+using SistemaBiblitecarioManas.Entities.Entities;
+using SistemaBiblitecarioManas.Entities.Interface;
+using System.Threading.Tasks;
 
 namespace SistemaBibliotecarioManas.Infra.Repository.PessoaRepository
 {
-    class PessoaRepository
+    public class PessoaRepository : GenericRepository<PessoaEntity>, IPessoaRepository
     {
+        public readonly MainContext _dbContext;
+
+        public PessoaRepository(MainContext context) : base(context) { }
+
+        public async Task<bool> PessoaJaExiste(string cpf)
+        {
+            return await _dbSet.AnyAsync(p => p.CPF == cpf && p.Deletado != true);
+        }
+        
+        public async Task<bool> PessoaJaExisteComEsseCpf(string cpf, int id)
+        {
+            return await _dbSet.AnyAsync(p => p.CPF == cpf && p.Id == id && p.Deletado != true);
+        }
     }
 }
