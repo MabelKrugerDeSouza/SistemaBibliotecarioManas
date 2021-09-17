b942341321ffe6001ce32a25f200dce949dfe12b
Mabel Kruger de Souza <mabelksouza@gmail.com>
2021-09-17 17:24:57 -0300

    Criação do application e implementacao do model base

---
+++ SistemaBibliotecarioManas/SistemaBibliotecarioManas.Service/Models/Pessoa/PessoaModelBase.cs
@@ -0,0 +1,17 @@
+﻿using SistemaBiblitecarioManas.Entities;
+using SistemaBiblitecarioManas.Entities.Complex_Type;
+using System;
+
+namespace SistemaBibliotecarioManas.Application.Models.Pessoa
+{
+    public class PessoaModelBase
+    {
+        public string NomePessoa { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+        public TipoPessoa TipoPessoa { get; set; }
+    }
+}
---
+++ SistemaBibliotecarioManas/SistemaBibliotecarioManas.Service/Service/Livro/IPessoaService.cs
@@ -0,0 +1,16 @@
+﻿using SistemaBiblitecarioManas.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SistemaBibliotecarioManas.Service
+{
+    public interface IPessoaService
+    {
+        Task<int> Create(PessoaRequestModel request);
+        Task<PessoaEntity> Update(int id, PessoaRequestModel request);
+        Task<PessoaEntity> Delete(int id);
+        Task<PessoaResponseModel> GetById(int id);
+        Task<List<PessoaResponseModel>> GetAll();
+    }
+}
