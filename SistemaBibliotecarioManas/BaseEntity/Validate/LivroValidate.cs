f4ca04a0d93333083672ef6c2101d5fc54df9954
Mabel Kruger de Souza <mabelksouza@gmail.com>
2021-09-18 22:44:35 -0300

    Continuação da implementação do service e implementação do Validate

---
+++ SistemaBibliotecarioManas/BaseEntity/Validate/LivroValidate.cs
@@ -0,0 +1,10 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaBiblitecarioManas.Entities.Validate
+{
+    class LivroValidate
+    {
+    }
+}
---
+++ SistemaBibliotecarioManas/SistemaBibliotecarioManas.Service/Service/Livro/LivroService.cs
@@ -0,0 +1,25 @@
+﻿using SistemaBibliotecarioManas.Application.Models.Livro;
+using SistemaBiblitecarioManas.Entities.Entities;
+using SistemaBiblitecarioManas.Entities.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaBibliotecarioManas.Application.Service.Livro
+{
+    public class LivroService : ILivroService
+    {
+        private readonly ILivroRepository _repository;
+        public LivroService(ILivroRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<int> Create(LivroRequestModel livroRequestModel)
+        {
+            var livro = new LivroEntity(livroRequestModel.NomeLivro, livroRequestModel.Id);
+
+        }
+    }
+}
